af552c95f764d26b294c7148397fc5b1a7161604
PepeKwapien <p.kwapien@op.pl>
2021-05-13 01:34:30 +0200

    Removed '2's from method names in algorithm

--- algorithm/MinMaxAlgorithm.java
+++ algorithm/MinMaxAlgorithm.java
@@ -13,17 +13,17 @@ public class MinMaxAlgorithm {
     }
 
     public int makeMove(int playersNumber, Board currentBoard){
-        return Max2(playersNumber, playersNumber, 0, currentBoard, maxDepth, false, true)[0];
+        return Max(playersNumber, playersNumber, 0, currentBoard, maxDepth, false, true)[0];
     }
 
-    private int[] Max2(int playersNumber, int currentPlayersNumber,
-                       int previousPocket, Board board, int remainingDepth, boolean repeatMove, boolean isInitial){
+    private int[] Max(int playersNumber, int currentPlayersNumber,
+                      int previousPocket, Board board, int remainingDepth, boolean repeatMove, boolean isInitial){
         ArrayList<Integer> availablePockets = board.getAvailableMovesForPlayer(currentPlayersNumber);
         Integer bestPath = null;
         int score = 0;
 
         if(repeatMove){
-            return Min2(playersNumber, (currentPlayersNumber + 1)%2, previousPocket, board,
+            return Min(playersNumber, (currentPlayersNumber + 1)%2, previousPocket, board,
                     remainingDepth - 1, false);
         }
         else{
@@ -36,7 +36,7 @@ public class MinMaxAlgorithm {
                     Board clonedBoard = board.clone();
                     boolean dontSwitchPlayer = clonedBoard.makeMove(currentPlayersNumber,
                             i + currentPlayersNumber*board.getNumOfPocketsForPlayer() - 1);
-                    int[] childScore = Min2(playersNumber, (currentPlayersNumber + 1)%2, i,
+                    int[] childScore = Min(playersNumber, (currentPlayersNumber + 1)%2, i,
                             clonedBoard, remainingDepth - (dontSwitchPlayer ? 0 : 1), dontSwitchPlayer);
 
                     if(bestPath == null || childScore[1] > score){
@@ -54,15 +54,15 @@ public class MinMaxAlgorithm {
         return new int[]{bestPath, score};
     }
 
-    private int[] Min2(int playersNumber, int currentPlayersNumber,
-                       int previousPocket, Board board, int remainingDepth, boolean repeatMove){
+    private int[] Min(int playersNumber, int currentPlayersNumber,
+                      int previousPocket, Board board, int remainingDepth, boolean repeatMove){
 
         ArrayList<Integer> availablePockets = board.getAvailableMovesForPlayer(currentPlayersNumber);
         Integer bestPath = null;
         int score = 0;
 
         if(repeatMove){
-            return Max2(playersNumber, (currentPlayersNumber + 1)%2, previousPocket, board,
+            return Max(playersNumber, (currentPlayersNumber + 1)%2, previousPocket, board,
                     remainingDepth - 1, false, false);
         }
         else{
@@ -74,7 +74,7 @@ public class MinMaxAlgorithm {
                     Board clonedBoard = board.clone();
                     boolean dontSwitchPlayer = clonedBoard.makeMove(currentPlayersNumber,
                             i + currentPlayersNumber*board.getNumOfPocketsForPlayer() - 1);
-                    int[] childScore = Max2(playersNumber, (currentPlayersNumber + 1)%2, i, clonedBoard,
+                    int[] childScore = Max(playersNumber, (currentPlayersNumber + 1)%2, i, clonedBoard,
                             remainingDepth - (dontSwitchPlayer ? 0 : 1), dontSwitchPlayer, false);
 
                     if(bestPath == null || childScore[1] < score){
